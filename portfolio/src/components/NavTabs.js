40f360f46a572519c385b3b928a868644525e573
Abe Omaits <aomaits@gmail.com>
2023-10-02 14:37:50 -0500

    First nav bar links now rendering

---
+++ portfolio/src/components/NavTabs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// Here we are using object destructuring assignment to pluck off our variables from the props object
+// We assign them to their own variable names
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#Portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer.js";
-
-const App = () => <PortfolioContainer />;
-
-export default App;
